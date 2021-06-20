88ddee8c3f15126f1c7c819cdb613e8db8d69160
Feliciano07 <fernandoxhajon122@gmail.com>
2021-06-19 23:33:46 -0600

    Google Sign added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const{request, response } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 
@@ -50,7 +51,53 @@ const login = async (req = request, res = response) =>{
 
 }
 
+const googleSingin = async(req = request, res = response) =>{
+    const {id_token} = req.body;
+
+    
+
+    try {
+        const {correo, nombre, img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        //Si el usuario no existe
+        if(!usuario){
+            const data = {
+                nombre,
+                correo,
+                password:':P',
+                img,
+                google:true
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //si el usuario en db
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg:'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        //generar jwt
+        const token = await generarJWT(usuario.id);
+        
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        })   
+    }
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSingin
 }
--- routers/auth.routes.js
+++ routers/auth.routes.js
@@ -4,7 +4,7 @@ const { check } = require('express-validator');
 const router = Router();
 
 const {
-login
+login, googleSingin
 } = require('../controllers/auth.controller');
 const { ValidarCampos } = require('../middlewares/validar-campos');
 
@@ -14,6 +14,11 @@ router.post('/login',[
     check('password', 'La contrasenia es obligatoria').not().isEmpty(),
     ValidarCampos
 ], login );
+
+router.post('/google',[
+    check('id_token', 'El id token es necesario').not().isEmpty(),
+    ValidarCampos
+], googleSingin );
 
 
 
